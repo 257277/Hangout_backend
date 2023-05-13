832e79a26954cf9bf5a308a3c1418518aef6c6bb
Rahul Sharma <40212890+257277@users.noreply.github.com>
2023-05-13 14:30:06 +0530

    Updating cors error

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 // <------------   Socket.io  ----------------->
 const http = require('http');
 const server = http.createServer(app);
