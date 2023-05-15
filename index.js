d4504df9b5c8d4285d909a93cefbe75e15d85f68
Rahul Sharma <40212890+257277@users.noreply.github.com>
2023-05-15 17:04:26 +0530

    up

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({ origin: "*" }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(cors({ origin: "*" }));
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
